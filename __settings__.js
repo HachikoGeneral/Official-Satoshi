942baafa30353dc6391ea3431f07126b9f69f6a9
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2025-02-02 16:50:55 +0000

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,24 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2105096.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 202483039, 202483054, 202483057, 202483046, 202483064, 202798647, 202996575, 204498356, 204526520, 204981564, 204982066, 205130228, 209683145 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/204551219/1/basis.wasm.js', 'wasmUrl' : 'files/assets/204551217/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/204551218/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/202483047/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/202483267/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/202483042/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/203056039/1/draco.wasm.js', 'wasmUrl' : 'files/assets/203056040/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/203056038/1/draco.js', 'preload' : true},
+];
